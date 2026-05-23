400956d000c2edfed04a2fd8554b30c612aab20d
Bharmal Ibrahim <ibrahim.growbharat@gmail.com>
2026-05-23 13:42:00 +0530

    all web

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.css"],
+  theme: {
+    extend: {
+      boxShadow: {
+        soft: "0 18px 60px rgba(15, 23, 42, 0.08)",
+      },
+    },
+  },
+  plugins: [],
+};
